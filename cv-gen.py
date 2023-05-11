0fa162707c8a635338a17c333c9991a390d63f46
WvdMeijs <wijnand.van.der.meijs@virtualsciences.nl>
2023-05-11 21:45:26 +0200

    working blue beam on the left side!

--- cv-gen.py
+++ cv-gen.py
@@ -4,6 +4,10 @@ class PDF(FPDF):
     def header(self):
         # Add logo image
         # self.image('logo.png', 10, 10, 33)
+        self.set_fill_color(23, 54, 93)
+        self.WIDTH = 210
+        self.HEIGHT = 297
+        self.rect(0, 0, 40, self.HEIGHT, 'F')
         # Set font and size for the header text
         self.set_font('Arial', 'B', 15)
         # Move to the right
@@ -17,7 +21,7 @@ class PDF(FPDF):
         # Set font for the personal info
         self.set_font('Arial', '', 12)
         # Add name
-        self.cell(0, 10, 'Your Name', 0, 1)
+        self.cell(0, 10, 'Wijnand van der Meijs', 0, 1)
         # Add address
         self.cell(0, 10, 'Your Address', 0, 1)
         # Add phone number
